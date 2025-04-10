4c9345b5d584b802c76175738a5b730e038a8d17
Timur Kalandarov <t1maccapp@gmail.com>
2025-04-10 14:21:44 +0500

    add args parser, show added accounts on ui

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,83 @@
 use std::sync::Arc;
 
 use anyhow::Result;
+use clap::Parser;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     layout::{Constraint, Direction, Layout},
     style::{Style, Stylize},
-    widgets::Block,
+    widgets::{Block, List},
     DefaultTerminal, Frame,
 };
+use tokio::sync::RwLock;
+
+#[derive(Parser)]
+pub struct Args {
+    #[clap(alias = "account", short, long, value_parser, num_args = 1..)]
+    pub accounts: Vec<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut terminal = ratatui::init();
-
     let state = Arc::new(State::default());
 
-    let app_result = App::default().run(&mut terminal, state.clone());
+    let args = Args::parse();
+
+    if args.accounts.len() < 1 {
+        anyhow::bail!("provide at least one --account login:password");
+    }
+
+    for account_str in args.accounts {
+        let parts: Vec<&str> = account_str.split(':').collect();
+
+        if parts.len() != 2 {
+            anyhow::bail!("invalid account format. Expected 'login:password'");
+        }
+
+        state
+            .add_account(parts[0].to_string(), parts[1].to_string())
+            .await;
+    }
+
+    let mut terminal = ratatui::init();
+
+    let app_result = App::default().run(&mut terminal, state.clone()).await;
 
     ratatui::restore();
 
     app_result
 }
 
+#[derive(Debug, Clone)]
+pub struct Account {
+    login: String,
+    password: String,
+}
+
 #[derive(Debug, Default)]
-pub struct State {}
+pub struct State {
+    pub accounts: RwLock<Vec<Account>>,
+}
+
+#[derive(Debug, Default)]
+pub struct TerminalState {
+    pub accounts: Vec<Account>,
+}
+
+impl State {
+    pub async fn add_account(&self, login: String, password: String) {
+        self.accounts
+            .write()
+            .await
+            .push(Account { login, password });
+    }
+
+    pub async fn as_terminal_state(&self) -> TerminalState {
+        TerminalState {
+            accounts: self.accounts.read().await.clone(),
+        }
+    }
+}
 
 #[derive(Debug, Default)]
 pub enum SelectedWidget {
@@ -36,20 +90,23 @@ pub enum SelectedWidget {
 #[derive(Debug, Default)]
 pub struct App {
     selected_widget: SelectedWidget,
+    state: TerminalState,
     exit: bool,
 }
 
 impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal, state: Arc<State>) -> Result<()> {
+    pub async fn run(&mut self, terminal: &mut DefaultTerminal, state: Arc<State>) -> Result<()> {
         while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
+            let terminal_state = state.as_terminal_state().await;
+
+            terminal.draw(|frame| self.draw(frame, terminal_state))?;
             self.handle_events()?;
         }
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
-        ui(frame, self)
+    fn draw(&self, frame: &mut Frame<'_>, terminal_state: TerminalState) {
+        ui(frame, self, terminal_state);
     }
 
     fn handle_events(&mut self) -> Result<()> {
@@ -89,7 +146,7 @@ impl App {
     }
 }
 
-pub fn ui(frame: &mut Frame, app: &App) {
+pub fn ui(frame: &mut Frame<'_>, app: &App, terminal_state: TerminalState) {
     let layout_main = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Ratio(8, 9), Constraint::Length(2)])
@@ -102,7 +159,7 @@ pub fn ui(frame: &mut Frame, app: &App) {
 
     let layout_left_tower = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([Constraint::Ratio(1, 9), Constraint::Ratio(8, 9)])
+        .constraints([Constraint::Ratio(2, 9), Constraint::Ratio(7, 9)])
         .split(layout_working_area[0]);
 
     let layout_rigth_tower = Layout::default()
@@ -111,24 +168,42 @@ pub fn ui(frame: &mut Frame, app: &App) {
         .split(layout_working_area[1]);
 
     let status_widget = Block::bordered();
-    let mut accounts_block = Block::bordered().title("[1] Accounts");
-    let mut folders_block = Block::bordered().title("[2] Folders");
-    let mut messages_block = Block::bordered().title("[3] Messages");
+    let accounts_block =
+        Block::bordered()
+            .title("[1] Accounts")
+            .border_style(match app.selected_widget {
+                SelectedWidget::Accounts => Style::new().green(),
+                _ => Style::default(),
+            });
+
+    let accounts_list = List::new(
+        terminal_state
+            .accounts
+            .iter()
+            .map(|a| a.login.to_string())
+            .collect::<Vec<String>>(),
+    )
+    .block(accounts_block);
+
+    let folders_block =
+        Block::bordered()
+            .title("[2] Folders")
+            .border_style(match app.selected_widget {
+                SelectedWidget::Folders => Style::new().green(),
+                _ => Style::default(),
+            });
+
+    let messages_block =
+        Block::bordered()
+            .title("[3] Messages")
+            .border_style(match app.selected_widget {
+                SelectedWidget::Messages => Style::new().green(),
+                _ => Style::default(),
+            });
+
     let ads_block = Block::bordered().title("Advertisement");
 
-    match app.selected_widget {
-        SelectedWidget::Accounts => {
-            accounts_block = accounts_block.border_style(Style::new().green()).bold()
-        }
-        SelectedWidget::Folders => {
-            folders_block = folders_block.border_style(Style::new().green()).bold()
-        }
-        SelectedWidget::Messages => {
-            messages_block = messages_block.border_style(Style::new().green()).bold()
-        }
-    };
-
-    frame.render_widget(accounts_block, layout_left_tower[0]);
+    frame.render_widget(accounts_list, layout_left_tower[0]);
     frame.render_widget(folders_block, layout_left_tower[1]);
     frame.render_widget(messages_block, layout_rigth_tower[0]);
     frame.render_widget(ads_block, layout_rigth_tower[1]);
