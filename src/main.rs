df92fde36a79cfe143f88c0ebd47110b52fdc7a5
Timur Kalandarov <t1maccapp@gmail.com>
2025-04-10 12:02:24 +0500

    add basic key pressed handlers, basic app state

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::sync::Arc;
+
 use anyhow::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     layout::{Constraint, Direction, Layout},
-    style::Style,
-    widgets::{Block, Borders},
+    style::{Style, Stylize},
+    widgets::Block,
     DefaultTerminal, Frame,
 };
 
@@ -11,7 +13,9 @@ use ratatui::{
 async fn main() -> Result<()> {
     let mut terminal = ratatui::init();
 
-    let app_result = App::default().run(&mut terminal);
+    let state = Arc::new(State::default());
+
+    let app_result = App::default().run(&mut terminal, state.clone());
 
     ratatui::restore();
 
@@ -19,12 +23,24 @@ async fn main() -> Result<()> {
 }
 
 #[derive(Debug, Default)]
+pub struct State {}
+
+#[derive(Debug, Default)]
+pub enum SelectedWidget {
+    Accounts,
+    Folders,
+    #[default]
+    Messages,
+}
+
+#[derive(Debug, Default)]
 pub struct App {
+    selected_widget: SelectedWidget,
     exit: bool,
 }
 
 impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal, state: Arc<State>) -> Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
             self.handle_events()?;
@@ -33,7 +49,7 @@ impl App {
     }
 
     fn draw(&self, frame: &mut Frame) {
-        ui(frame)
+        ui(frame, self)
     }
 
     fn handle_events(&mut self) -> Result<()> {
@@ -49,8 +65,23 @@ impl App {
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
+            KeyCode::Char('1') => self.select_accounts_widget(),
+            KeyCode::Char('2') => self.select_folders_widget(),
+            KeyCode::Char('3') => self.select_messages_widget(),
             _ => {}
         }
+    }
+
+    fn select_accounts_widget(&mut self) {
+        self.selected_widget = SelectedWidget::Accounts
+    }
+
+    fn select_folders_widget(&mut self) {
+        self.selected_widget = SelectedWidget::Folders
+    }
+
+    fn select_messages_widget(&mut self) {
+        self.selected_widget = SelectedWidget::Messages
     }
 
     fn exit(&mut self) {
@@ -58,7 +89,7 @@ impl App {
     }
 }
 
-pub fn ui(frame: &mut Frame) {
+pub fn ui(frame: &mut Frame, app: &App) {
     let layout_main = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Ratio(8, 9), Constraint::Length(2)])
@@ -80,22 +111,26 @@ pub fn ui(frame: &mut Frame) {
         .split(layout_working_area[1]);
 
     let status_widget = Block::bordered();
+    let mut accounts_block = Block::bordered().title("[1] Accounts");
+    let mut folders_block = Block::bordered().title("[2] Folders");
+    let mut messages_block = Block::bordered().title("[3] Messages");
+    let ads_block = Block::bordered().title("Advertisement");
 
-    let accounts_block = Block::bordered().title("[1] Accounts");
-
-    let folders_block = Block::bordered().title("[2] Folders");
-
-    let messages_block = Block::bordered().title("[3] Messages");
-
-    let ads_block = Block::bordered().title("[4] Ads");
+    match app.selected_widget {
+        SelectedWidget::Accounts => {
+            accounts_block = accounts_block.border_style(Style::new().green()).bold()
+        }
+        SelectedWidget::Folders => {
+            folders_block = folders_block.border_style(Style::new().green()).bold()
+        }
+        SelectedWidget::Messages => {
+            messages_block = messages_block.border_style(Style::new().green()).bold()
+        }
+    };
 
     frame.render_widget(accounts_block, layout_left_tower[0]);
-
     frame.render_widget(folders_block, layout_left_tower[1]);
-
     frame.render_widget(messages_block, layout_rigth_tower[0]);
-
     frame.render_widget(ads_block, layout_rigth_tower[1]);
-
     frame.render_widget(status_widget, layout_main[1]);
 }
