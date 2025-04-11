612d0097e41124359917cd1fc6eaa994e2fe0b3a
Timur Kalandarov <t1maccapp@gmail.com>
2025-04-11 18:13:56 +0500

    refactor more, add email backend

---
+++ src/args.rs
@@ -0,0 +1,37 @@
+use std::sync::Arc;
+
+use crate::state::State;
+
+use clap::Parser;
+
+use anyhow::Result;
+
+#[derive(Parser)]
+pub struct Args {
+    #[clap(alias = "account", short, long, value_parser, num_args = 1..)]
+    pub accounts: Vec<String>,
+}
+
+pub async fn get_initial_state_from_args() -> Result<Arc<State>> {
+    let state = Arc::new(State::default());
+
+    let args = Args::parse();
+
+    if args.accounts.len() < 1 {
+        anyhow::bail!("provide at least one --account login:password");
+    }
+
+    for account_str in args.accounts {
+        let parts: Vec<&str> = account_str.split(':').collect();
+
+        if parts.len() != 2 {
+            anyhow::bail!("invalid account format. Expected 'login:password'");
+        }
+
+        state
+            .add_account(parts[0].to_string(), parts[1].to_string())
+            .await;
+    }
+
+    Ok(state)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,18 @@
 mod app;
+mod args;
+mod email;
 mod state;
 mod ui;
 
-use std::sync::Arc;
-
 use anyhow::Result;
 use app::App;
-use clap::Parser;
-use state::State;
-
-#[derive(Parser)]
-pub struct Args {
-    #[clap(alias = "account", short, long, value_parser, num_args = 1..)]
-    pub accounts: Vec<String>,
-}
+use args::get_initial_state_from_args;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let state = Arc::new(State::default());
-
-    let args = Args::parse();
-
-    if args.accounts.len() < 1 {
-        anyhow::bail!("provide at least one --account login:password");
-    }
-
-    for account_str in args.accounts {
-        let parts: Vec<&str> = account_str.split(':').collect();
-
-        if parts.len() != 2 {
-            anyhow::bail!("invalid account format. Expected 'login:password'");
-        }
-
-        state
-            .add_account(parts[0].to_string(), parts[1].to_string())
-            .await;
-    }
-
     let mut terminal = ratatui::init();
+
+    let state = get_initial_state_from_args().await?;
 
     let app_result = App::default().run(&mut terminal, state.clone()).await;
 
